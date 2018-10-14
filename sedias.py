9553dd955cdd96135e8a88c35fe4a13d2ec81d79
András Jankó <dandrid2@gmail.com>
2018-10-14 15:26:03 +0200

    One-hot encoded DataFrames

--- sedias.py
+++ sedias.py
@@ -21,7 +21,7 @@ class RAVDESS:
             ("disgust", "07"), 
             ("surprised", "08")])
         self.emotion_intensities = np.array([("normal", "01"), ("strong", "02")])
-        self.statements = np.array([("Kids are talking by the door", "01"), ("Dogs are sitting by the door", "02")])
+        self.statements = np.array([("Kids_are_talking_by_the_door", "01"), ("Dogs_are_sitting_by_the_door", "02")])
         self.repetitions = np.array(["01", "02"])
         self.actors = np.array([
             ("male", "01"), 
@@ -97,14 +97,17 @@ class RAVDESS:
         df = pd.DataFrame(  # contruct DataFrame from data
             { 
                 'emotion'           : pd.Categorical([row[0] for row in data]),
-                'emotion intensity' : pd.Categorical([row[1] for row in data]),
+                'emotion_intensity' : pd.Categorical([row[1] for row in data]),
                 'statement'         : pd.Categorical([row[2] for row in data]),
                 'repetition'        : pd.Categorical([row[3] for row in data]),
                 'actor'             : pd.Categorical([row[4] for row in data]),
-                'actor gender'      : pd.Categorical([row[5] for row in data]),
+                'actor_gender'      : pd.Categorical([row[5] for row in data]),
                 'samplerate'        : pd.Categorical([row[6] for row in data]),
-                'audio data'        : pd.Series([row[7] for row in data])
+                'audio_data'        : pd.Series([row[7] for row in data])
             })
+
+        # one-hot encode columns
+        df = pd.get_dummies(df, columns=["emotion", "emotion_intensity", "statement", "repetition", "actor", "actor_gender", "samplerate"])
         return df
 
     def addSepartors(self, fragments):
@@ -125,16 +128,16 @@ class EMODB:
             ("male", "15"), 
             ("female", "16")])
         self.statements = np.array([
-            ("Der Lappen liegt auf dem Eisschrank.", "a01"), 
-            ("Das will sie am Mittwoch abgeben.", "a02"),
-            ("Heute abend könnte ich es ihm sagen.", "a04"), 
-            ("Das schwarze Stück Papier befindet sich da oben neben dem Holzstück.", "a05"),
-            ("In sieben Stunden wird es soweit sein.", "a07"), 
-            ("Was sind denn das für Tüten, die da unter dem Tisch stehen?", "b01"),
-            ("Sie haben es gerade hochgetragen und jetzt gehen sie wieder runter.	", "b02"), 
-            ("An den Wochenenden bin ich jetzt immer nach Hause gefahren und habe Agnes besucht.", "b03"),
-            ("Ich will das eben wegbringen und dann mit Karl was trinken gehen.", "b09"), 
-            ("Die wird auf dem Platz sein, wo wir sie immer hinlegen.", "b10")])
+            ("Der_Lappen_liegt_auf_dem_Eisschrank.", "a01"), 
+            ("Das_will_sie_am_Mittwoch_abgeben.", "a02"),
+            ("Heute_abend_könnte_ich_es_ihm_sagen.", "a04"), 
+            ("Das_schwarze_Stück_Papier_befindet_sich_da_oben_neben_dem_Holzstück.", "a05"),
+            ("In_sieben_Stunden_wird_es_soweit_sein.", "a07"), 
+            ("Was_sind_denn_das_für_Tüten,_die_da_unter_dem_Tisch_stehen?", "b01"),
+            ("Sie_haben_es_gerade_hochgetragen_und_jetzt_gehen_sie_wieder_runter.", "b02"), 
+            ("An_den_Wochenenden_bin_ich_jetzt_immer_nach_Hause_gefahren_und_habe_Agnes_besucht.", "b03"),
+            ("Ich_will_das_eben_wegbringen_und_dann_mit_Karl_was_trinken_gehen.", "b09"), 
+            ("Die_wird_auf_dem_Platz_sein,_wo_wir_sie_immer_hinlegen.", "b10")])
         self.emotions = np.array([
             ("neutral", "N"), 
             ("anger", "W"), # Wut
@@ -177,22 +180,27 @@ class EMODB:
                 'statement'         : pd.Categorical([row[1] for row in data]),
                 'repetition'        : pd.Categorical([row[2] for row in data]),
                 'actor'             : pd.Categorical([row[3] for row in data]),
-                'actor gender'      : pd.Categorical([row[4] for row in data]),
+                'actor_gender'      : pd.Categorical([row[4] for row in data]),
                 'samplerate'        : pd.Categorical([row[5] for row in data]),
-                'audio data'        : pd.Series([row[6] for row in data])
+                'audio_data'        : pd.Series([row[6] for row in data])
             })
+
+        # one-hot encode columns
+        df = pd.get_dummies(df, columns=["emotion", "statement", "repetition", "actor", "actor_gender", "samplerate"])
         return df
 
 
+pd.set_option("max_columns" , None)
 
 print("RAVDESS:")
 ravdess = RAVDESS()
 df_ravdess = ravdess.createDataFrame()
 print("Date frame size: " + str(len(df_ravdess)))
-
+print(df_ravdess.head())
 
 print("EMODB:")
 
 emodb = EMODB()
 df_emodb = emodb.createDataFrame()
 print("Date frame size: " + str(len(df_emodb)))
+print(df_emodb.head())
